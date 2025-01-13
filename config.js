e049d26b3915cf180c7fb056fda748f37a5d199c
Thekonator <150165828+Thekonator@users.noreply.github.com>
2025-01-13 18:18:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,23 +3,23 @@ require("dotenv").config(); // Load environment variables
 module.exports = {
     prefix: process.env.PREFIX || ".", // Command prefix
 
-    ownerName: process.env.OWNER_NAME || "ISHMAEL", // Owner name
+    ownerName: process.env.OWNER_NAME || "ACE 10k", // Owner name
 
-    ownerNumber: process.env.OWNER_NUMBER || "27767375884", // Your WhatsApp number
+    ownerNumber: process.env.OWNER_NUMBER || "2349168764213", // Your WhatsApp number
 
-    sudo: process.env.SUDO ? process.env.SUDO.split(",") : ["27767375884"," 27767375884"], // People Who can use the bot even in Private mode
+    sudo: process.env.SUDO ? process.env.SUDO.split(",") : ["2349168764213"," 2349168764213"], // People Who can use the bot even in Private mode
 
     mode: process.env.MODE || "private", // Bot mode: 'public' or 'private'
 
-    region: process.env.REGION || "South Africa", // Region
+    region: process.env.REGION || "Nigeria", // Region
 
     botName: process.env.BOT_NAME || "Rias Gremory V3", // Bot name
 
     exifPack: process.env.EXIF_PACK || "RIAS V3 LOVES", // Sticker pack name
 
-    exifAuthor: process.env.EXIF_AUTHOR || "Ishmael", // Author of the sticker pack
+    exifAuthor: process.env.EXIF_AUTHOR || "ACE 10k", // Author of the sticker pack
 
-    timeZone: process.env.TIME_ZONE || "Africa/Limpopo", // Time zone
+    timeZone: process.env.TIME_ZONE || "Africa/Lagos", // Time zone
 
     presenceStatus: process.env.PRESENCE_STATUS || "recording", // Bot presence status
 
